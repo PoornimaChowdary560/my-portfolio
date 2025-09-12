5e5f30764c06892e508e403cce4f83b0cebcaff6
poornima maddipat <poornimamaddipati25@gmail.com>
2025-09-12 10:44:45 +0530

    added resume

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -50,7 +50,7 @@ const Navbar = () => {
         </li>
         <li>
           <a
-            href="/resume.pdf"
+            href="/Poornima_Resume.pdf"
             download
             className="flex items-center bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded-md"
           >
